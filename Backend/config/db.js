98fe6c1ce939574eff0af5ddddb6e058e22254c5
Ajay joseph <aj2506039@gmail.com>
2025-09-03 09:56:55 +0530

    feat:changed mongourl

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,8 +1,12 @@
 import mongoose from "mongoose";
-const  connectDB=()=>{
-    mongoose.connect(process.env.MONGO_URI)
-    .then(()=>console.log('mongoose connected'))
-    .catch(()=>console.log('mongoose not connected'))
-}
+import dotenv from "dotenv";
 
-export default connectDB;
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected ✅"))
+  .catch((err) => console.error("MongoDB connection error ❌:", err));
