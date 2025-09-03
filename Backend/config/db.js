d719f1c5eaae4c19109819053d2ff04f84b118a9
Ajay joseph <aj2506039@gmail.com>
2025-09-03 10:06:02 +0530

    feat:conntected

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 const  connectDB=()=>{
     mongoose.connect(process.env.MONGO_URI)
     .then(()=>console.log('mongoose connected'))
-    .catch(()=>console.log('mongoose not connected',err.message))
+    .catch(()=>console.log('mongoose not connected'))
 }
 
 export default connectDB;
