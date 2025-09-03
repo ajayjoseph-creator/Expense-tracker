1d9e4e9c58127f22f5417b562ccf9c9765864fef
Ajay joseph <aj2506039@gmail.com>
2025-09-03 10:37:27 +0530

    feat:schema updated

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  username: {type: String,required: true},
-  email: {type: String,required: true},
-  password: {type: String,required: true,}
+  username: {type: String},
+  email: {type: String},
+  password: {type: String}
 }, { timestamps: true });
 const User = mongoose.model("User", userSchema);
 export default User;
